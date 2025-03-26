4eab7c3c92d03a7a4c03af17b94298e81faf1d28
V4d1m3 <vapelikh12@gmail.com>
2025-03-26 17:42:49 +0300

    border-animation, changed border-radius calculation

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,39 @@
 window.addEventListener('load', () => {
     const mainFieldBody = document.querySelector('#mainFieldBody')
+    const section = document.querySelector('section')
     const heightF = getComputedStyle(mainFieldBody).height
     mainFieldBody.style.setProperty('--heightF', heightF)
+    section.style.setProperty('--heightF', heightF)
 })
 
 let input = document.getElementById('inputField')
 let buttons = document.getElementById('actionsBody')
 
-document.addEventListener('keydown', clickOrPress)
+// document.addEventListener('keydown', clickOrPress)
 
 buttons.addEventListener('click', clickOrPress)
 
 function clickOrPress(event) {
-    if (event.type === 'keydown') {
-        const key = event.key
-        if (
-            [
-                '0',
-                '1',
-                '2',
-                '3',
-                '4',
-                '5',
-                '6',
-                '7',
-                '8',
-                '9' /*, '.'*/,
-            ].includes(key)
-        ) {
-            input.value += key
-        }
-    } else if (event.target.tagName === 'BUTTON') {
+    // if (event.type === 'keydown') {
+    //     const key = event.key
+    //     if (
+    //         [
+    //             '0',
+    //             '1',
+    //             '2',
+    //             '3',
+    //             '4',
+    //             '5',
+    //             '6',
+    //             '7',
+    //             '8',
+    //             '9' /*, '.'*/,
+    //         ].includes(key)
+    //     ) {
+    //         input.value += key
+    //     }
+    // } else
+    if (event.target.tagName === 'BUTTON') {
         const action = event.target.dataset.action
         console.log(`Made action: ${action}`) //Шаблонная строка
 
